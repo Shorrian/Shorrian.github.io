efdd5e650844e9a32f74e06f3fab2497e98bcb6b
Harrison Ding <harrisond2015@gmail.com>
2015-11-08 02:00:28 -0600

    Added my about page and created a template for each html page so that the header will be capable of appearing on each page with just a couple of imports per page

--- shorrian.js
+++ shorrian.js
@@ -1,5 +1,8 @@
-$('.nav-buttons').click(function(){
-    $('.nav-buttons').hide("slow", function(){
+$('#clickme').click(function(){
+    $('#test').hide("slow", function(){
         alert("Animation complete");
     });
+});
+$('#about-me-button').click(function(){
+
 });
